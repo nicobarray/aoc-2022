fdb1658ba3172e0570c8807e22d7f260188627cd
Nicolas Barray <nbarray@gmail.com>
2022-12-05 08:52:04 +0100

    day5,part1

---
+++ src/days/5/solve.js
@@ -0,0 +1,47 @@
+export function part1(input) {
+  const lines = input.split("\n");
+
+  // initialize the stacks data.
+
+  let cratesLines = [];
+  let lineIndex = 0;
+  while (!lines[lineIndex].startsWith(" 1 ")) {
+    let line = lines[lineIndex];
+    cratesLines.push(line);
+    lineIndex++;
+  }
+  //   cratesLines.reverse();
+
+  const stacksCount = lines[lineIndex]
+    .split("   ")
+    .map((n) => parseInt(n, 10))
+    .reduce((a, b) => Math.max(a, b), -1);
+
+  const stacks = [...new Array(stacksCount)].map((_) => []);
+
+  for (let cratesLine of cratesLines) {
+    for (let i = 0; i < stacks.length; i++) {
+      const n = cratesLine.substring(i * 4, i * 4 + 4)[1];
+      if (n !== " ") {
+        stacks[i].push(n);
+      }
+    }
+  }
+
+  // move crates.
+  lineIndex += 2;
+  for (; lineIndex < lines.length; lineIndex++) {
+    const op = lines[lineIndex];
+
+    let [_move, times, _from, origin, _to, dest] = op.split(" ").map(Number);
+
+    while (times > 0) {
+      times--;
+      let [crate, ...rest] = stacks[origin - 1];
+      stacks[origin - 1] = rest;
+      stacks[dest - 1] = [crate, ...stacks[dest - 1]];
+    }
+  }
+
+  return stacks.map((s) => s[0]).join("");
+}
